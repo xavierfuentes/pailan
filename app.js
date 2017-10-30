5d66ea24a140d39d4a466483257c3d72971250bc
Xavi <xavifuefer@gmail.com>
2017-10-30 00:34:18 +0000

    add style for login page

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use((req, res, next) => {
   // After successful login, redirect back to the intended page
   if (
     !req.user &&
-    req.path !== '/login' &&
+    req.path !== '/signin' &&
     req.path !== '/signup' &&
     !req.path.match(/^\/auth/) &&
     !req.path.match(/\./)
@@ -125,8 +125,8 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
+app.get('/signin', userController.getSignin);
+app.post('/signin', userController.postSignin);
 app.get('/logout', userController.logout);
 app.get('/forgot', userController.getForgot);
 app.post('/forgot', userController.postForgot);
@@ -185,27 +185,27 @@ app.post('/services/add', passportConfig.isAuthenticated, serviceController.addS
  * OAuth authentication routes. (Sign in)
  */
 app.get('/auth/instagram', passport.authenticate('instagram'));
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 
@@ -221,11 +221,11 @@ app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect:
   res.redirect('/api/tumblr');
 });
 app.get('/auth/steam', passport.authorize('openid', { state: 'SOME STATE' }));
-app.get('/auth/steam/callback', passport.authorize('openid', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/steam/callback', passport.authorize('openid', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/pinterest', passport.authorize('pinterest', { scope: 'read_public write_public' }));
-app.get('/auth/pinterest/callback', passport.authorize('pinterest', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/pinterest/callback', passport.authorize('pinterest', { failureRedirect: '/signin' }), (req, res) => {
   res.redirect('/api/pinterest');
 });
 
--- controllers/user.js
+++ controllers/user.js
@@ -5,23 +5,23 @@ const passport = require('passport');
 const User = require('../models/User');
 
 /**
- * GET /login
- * Login page.
+ * GET /signin
+ * Signin page.
  */
-exports.getLogin = (req, res) => {
+exports.getSignin = (req, res) => {
   if (req.user) {
     return res.redirect('/services');
   }
-  res.render('account/login', {
-    title: 'Login',
+  res.render('account/signin', {
+    title: 'Sign in',
   });
 };
 
 /**
- * POST /login
+ * POST /signin
  * Sign in using email and password.
  */
-exports.postLogin = (req, res, next) => {
+exports.postSignin = (req, res, next) => {
   req.assert('email', 'Email is not valid').isEmail();
   req.assert('password', 'Password cannot be blank').notEmpty();
   req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
@@ -30,7 +30,7 @@ exports.postLogin = (req, res, next) => {
 
   if (errors) {
     req.flash('errors', errors);
-    return res.redirect('/login');
+    return res.redirect('/signin');
   }
 
   passport.authenticate('local', (err, user, info) => {
@@ -39,7 +39,7 @@ exports.postLogin = (req, res, next) => {
     }
     if (!user) {
       req.flash('errors', info);
-      return res.redirect('/login');
+      return res.redirect('/signin');
     }
     req.logIn(user, err => {
       if (err) {
@@ -69,7 +69,7 @@ exports.getSignup = (req, res) => {
     return res.redirect('/');
   }
   res.render('account/signup', {
-    title: 'Create Account',
+    title: 'Sign up',
   });
 };
 
