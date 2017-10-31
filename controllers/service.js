d2d356ece6e59f63bdcbd2285a7fb3710755c091
Xavi <xavifuefer@gmail.com>
2017-10-31 01:38:07 +0000

    add title to service pages

--- controllers/service.js
+++ controllers/service.js
@@ -10,7 +10,7 @@ exports.getActiveServices = (req, res) => {
   Service.find()
     .allPrivateByUser(req.user.id)
     .exec((err, services) => {
-      res.render('services/list', { services });
+      res.render('services/list', { title: 'Services', services });
     });
 };
 
@@ -18,7 +18,7 @@ exports.getServicesAvailable = (req, res) => {
   Service.find()
     .allDefault()
     .exec((err, services) => {
-      res.render('services/add', { services });
+      res.render('services/add', { title: 'Add Service', services });
     });
 };
 
