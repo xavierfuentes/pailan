50462bd8256f5ac2670ff40ba9ed160e660770e0
Xavi <xavifuefer@gmail.com>
2017-11-02 00:22:36 +0000

    style add service page

--- public/js/main.js
+++ public/js/main.js
@@ -8,16 +8,26 @@ $(function() {
   var userDataForm = $('add-service-form');
   var serviceIdInput = $('input#selectedServiceId');
   var AddServiceButton = $('#add-service-button');
-  var ServiceLogos = $('#add-service').find('.service-logo');
+  var ServiceLogos = $('#add-service').find('.default-service-box');
   var UserInput = $('#user');
   var PasswordInput = $('#password');
+  var ServiceInput = $('#service');
 
   // Events
   ServiceLogos.click(function(event) {
+    console.log($(this))
+    // put the id in the hidden input
     serviceSelectedId = $(this).data('id');
     serviceIdInput.val(serviceSelectedId);
+
+    // put the service's name in the input
+    ServiceInput.val($(this).data('name'));
+
+    // enable the form to be used
     UserInput.prop('disabled', false);
     PasswordInput.prop('disabled', false);
+
+    // toggles active class
     ServiceLogos.removeClass('active');
     $(this).addClass('active');
   })
